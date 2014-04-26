58b932b779cd544e3088b6d33d200b0e970ad339
Marjie <marjie.volk@gmail.com>
2014-04-26 15:08:17 -0700

    NodeMenu now instantiates buttons once and hides them

--- UnityProject/Assets/Graph/Nodes/NodeMenu.cs
+++ UnityProject/Assets/Graph/Nodes/NodeMenu.cs
@@ -7,12 +7,33 @@ public class NodeMenu : MonoBehaviour {
 	public bool isShown { get; private set; }
 
 	private bool isActionSelected = false;
-	private List<GameObject> createdButtons;
+	private List<GameObject> buttons;
 
 	// Use this for initialization
 	void Start () {
-		createdButtons = new List<GameObject>();
-		isShown = false;
+		buttons = new List<GameObject>();
+
+		Action[] actions = this.gameObject.GetComponents<Action>();
+		float angle = 90;
+		float dAngle = 360.0f / 5;
+		foreach (Action a in actions) {
+			GameObject button = a.getButton();
+			
+			float y = Mathf.Sin((angle * Mathf.PI) / 180.0f);
+			float x = Mathf.Cos((angle * Mathf.PI) / 180.0f);
+			Vector3 offset = new Vector3(x, y, 0);
+			offset.Normalize();
+			offset *= 0.8f;
+			
+			GameObject realButton = (GameObject) Instantiate(button, transform.position + offset, Quaternion.identity);
+			//realButton.transform.parent = this.gameObject.transform;
+			realButton.GetComponent<ActionButton>().setMenu(this);
+			buttons.Add(realButton);
+			
+			angle -= dAngle;
+		}
+
+		hide();
 	}
 	
 	// Update is called once per frame
@@ -21,38 +42,17 @@ public class NodeMenu : MonoBehaviour {
 	}
 
 	public void show() {
-		if (isShown) {
-			return;
-		}
-
 		isShown = true;
-		Action[] actions = this.gameObject.GetComponents<Action>();
-		float angle = 90;
-		float dAngle = 360.0f / 5;
-		foreach (Action a in actions) {
-			GameObject button = a.getButton();
-
-			float y = Mathf.Sin((angle * Mathf.PI) / 180.0f);
-			float x = Mathf.Cos((angle * Mathf.PI) / 180.0f);
-			Vector3 offset = new Vector3(x, y, 0);
-			offset.Normalize();
-			offset *= 0.8f;
-
-			GameObject realButton = (GameObject) Instantiate(button, transform.position + offset, Quaternion.identity);
-			//realButton.transform.parent = this.gameObject.transform;
-			realButton.GetComponent<ActionButton>().setMenu(this);
-			createdButtons.Add(realButton);
-
-			angle -= dAngle;
+		foreach (GameObject obj in buttons) {
+			obj.SetActive(true);
 		}
 	}
 
 	public void hide() {
 		isShown = false;
-		foreach (GameObject obj in createdButtons) {
-			Destroy(obj);
+		foreach (GameObject obj in buttons) {
+			obj.SetActive(false);
 		}
-		createdButtons.Clear();
 	}
 
 	public void setActionSelected(bool isActionSelected) {
