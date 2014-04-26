ef5ade105955f14162aa2aa672368e6fb1d19219
Marjie <marjie.volk@gmail.com>
2014-04-26 16:39:55 -0700

    Added owner sprites to NodeData

--- UnityProject/Assets/Graph/Highlightable.cs
+++ UnityProject/Assets/Graph/Highlightable.cs
@@ -8,8 +8,9 @@ public class Highlightable : MonoBehaviour {
     public delegate void OnClickHandler();
     public event OnClickHandler OnClicked;
 
-	private Sprite normalSprite;
+	protected Sprite normalSprite;
 	private SpriteRenderer spriteRenderer;
+	private bool isHighlighted = false;
 
 	// Use this for initialization
 	protected virtual void Start () {
@@ -21,12 +22,28 @@ public class Highlightable : MonoBehaviour {
 	void Update () {
 	
 	}
+
+	public void setNormalSprite(Sprite newNormal) {
+		normalSprite = newNormal;
+		if (!isHighlighted) {
+			setUnhighlighted();
+		}
+	}
+
+	public void setHighlightedSprite(Sprite newHighlight) {
+		highlightSprite = newHighlight;
+		if (isHighlighted) {
+			setHighlighted();
+		}
+	}
 	
 	public void setHighlighted() {
+		isHighlighted = true;
 		spriteRenderer.sprite = highlightSprite;
 	}
 	
 	public void setUnhighlighted() {
+		isHighlighted = false;
 		spriteRenderer.sprite = normalSprite;
 	}
 
