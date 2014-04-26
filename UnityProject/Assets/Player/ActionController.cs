2cbd0ddee4c2a14acfc716b6ad9ae31164fad35a
knexer <knexer@gmail.com>
2014-04-26 12:15:43 -0700

    Start of action lifecycle work

--- UnityProject/Assets/Player/ActionController.cs
+++ UnityProject/Assets/Player/ActionController.cs
@@ -8,6 +8,27 @@ namespace Assets.Player
 {
     public class ActionController : MonoBehaviour
     {
+        private Action selectedAction;
+
+        public void selectAction(Action toSelect)
+        {
+            PlayerData CurrentPlayer = this.GetComponent<TurnController>().CurrentPlayer;
+            //if the action is targeted
+            //ask for targets
+            if (toSelect.isTargeting)
+            {
+                //highlight the possible targets
+                toSelect.getPossibleTargets().ForEach
+                //attach event handlers to the possible targets so they're selected when they're clicked
+                //make a cancel button, attach an event handler to it
+
+                return;
+            }
+            
+            //no target needed
+            //add the action to the player
+            CurrentPlayer.scheduleAction(toSelect);
+        }
 
         // Use this for initialization
         void Start()
