8c58287afbc1ab40b70903a9f3a6c15b24627857
knexer <knexer@gmail.com>
2014-04-26 11:31:34 -0700

    Add basic functionality for switching visibility

---
+++ UnityProject/Assets/Player/VisibilityController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+
+namespace Assets.Player
+{
+    public class VisibilityController : MonoBehaviour
+    {
+        public delegate void VisibilityChangeHandler(Visibility visibility);
+
+        public event VisibilityChangeHandler VisibilityChanged;
+
+        public Visibility visibility { get; private set; }
+
+        void OnGUI()
+        {
+            if (GUI.Button(new Rect(110, 10, 100, 90), "Toggle Visibility"))
+            {
+                ToggleVisibility();
+            }
+            GUI.TextArea(new Rect(110, 110, 100, 90), visibility.ToString());
+        }
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+        public void setVisibility(Visibility newVisibility)
+        {
+            if (newVisibility != visibility) ToggleVisibility();
+        }
+
+        private void ToggleVisibility()
+        {
+            if (Visibility.Public == visibility) visibility = Visibility.Private;
+            else visibility = Visibility.Public;
+            
+            //notify everything that must be notified of the change
+            if (null != VisibilityChanged) VisibilityChanged(visibility);
+        }
+
+        public enum Visibility
+        {
+            Public = 0, Private=1
+        }
+    }
+}
