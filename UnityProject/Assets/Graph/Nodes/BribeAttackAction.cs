9cfa36817d76cfd8bd11c80357bed90f741dc27e
Marjie <marjie.volk@gmail.com>
2014-04-26 12:58:40 -0700

    Added first action subclass

---
+++ UnityProject/Assets/Graph/Nodes/BribeAttackAction.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BribeAttackAction : Action {
+
+	// Use this for initialization
+	void Start () {
+		isTargeting = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	override public List<Highlightable> getPossibleTargets() {
+		return null;
+	}
+	
+	override public bool scheduleUse(Highlightable target) {
+		return false;
+	}
+	
+	override public void clearScheduledUse() {
+
+	}
+	
+	override public void Activate() {
+
+	}
+}
