57ad9ff5d14d0e171c66c29b09afdd577499642c
knexer <knexer@gmail.com>
2014-04-26 11:32:11 -0700

    Created controller to manage action lifecycles

---
+++ UnityProject/Assets/Player/ActionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Player
+{
+    public class ActionController : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
