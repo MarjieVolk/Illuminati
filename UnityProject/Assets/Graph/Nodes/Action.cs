3b9780b5c19edf31bb4e12020ea9e4c18142c136
knexer <knexer@gmail.com>
2014-04-26 12:56:26 -0700

    Changed Action API

--- UnityProject/Assets/Graph/Nodes/Action.cs
+++ UnityProject/Assets/Graph/Nodes/Action.cs
@@ -1,31 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 public abstract class Action : MonoBehaviour {
 
 	public bool isTargeting;
 	public GameObject button;
 
-	// Use this for initialization
-	void Start () {
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    protected bool isScheduled;
+    protected Highlightable target;
 
 	public abstract List<Highlightable> getPossibleTargets();
-
-	public abstract bool scheduleUse(Highlightable target);
-
-	public abstract void clearScheduledUse();
 
 	public GameObject getButton() {
 		return button;
 	}
 
     public abstract void Activate();
+
+    public bool SetScheduled(Highlightable target)
+    {
+        if (isTargeting && null == target) return false;
+        if (!isTargeting && null != target) return false;
+        if (isTargeting && !getPossibleTargets().Contains(target)) return false;
+
+        this.target = target;
+        isScheduled = true;
+
+        return true;
+    }
+
+    public void clearScheduled()
+    {
+        target = null;
+        isScheduled = false;
+    }
 }
