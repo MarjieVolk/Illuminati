2ff19d7ce62bd1f21e30b9cd22c92687a69a977a
Marjie <marjie.volk@gmail.com>
2014-04-26 14:49:15 -0700

    Fixing up node menu a bit more - hide still doesn't work

--- UnityProject/Assets/Graph/Nodes/NodeMenu.cs
+++ UnityProject/Assets/Graph/Nodes/NodeMenu.cs
@@ -4,13 +4,15 @@ using System.Collections.Generic;
 
 public class NodeMenu : MonoBehaviour {
 
-	bool isActionSelected = false;
+	public bool isShown { get; private set; }
 
-	private List<Object> createdButtons;
+	private bool isActionSelected = false;
+	private List<GameObject> createdButtons;
 
 	// Use this for initialization
 	void Start () {
-		createdButtons = new List<Object>();
+		createdButtons = new List<GameObject>();
+		isShown = false;
 	}
 	
 	// Update is called once per frame
@@ -19,24 +21,35 @@ public class NodeMenu : MonoBehaviour {
 	}
 
 	public void show() {
+		if (isShown) {
+			return;
+		}
+
+		isShown = true;
 		Action[] actions = this.gameObject.GetComponents<Action>();
 		float angle = 90;
 		float dAngle = 360.0f / 5;
 		foreach (Action a in actions) {
 			GameObject button = a.getButton();
-			button.GetComponent<ActionButton>().setMenu(this);
+
 			float y = Mathf.Sin((angle * Mathf.PI) / 180.0f);
 			float x = Mathf.Cos((angle * Mathf.PI) / 180.0f);
 			Vector3 offset = new Vector3(x, y, 0);
 			offset.Normalize();
 			offset *= 0.8f;
-			createdButtons.Add(Instantiate(button, transform.position + offset, Quaternion.identity));
+
+			GameObject realButton = (GameObject) Instantiate(button, transform.position + offset, Quaternion.identity);
+			//realButton.transform.parent = this.gameObject.transform;
+			realButton.GetComponent<ActionButton>().setMenu(this);
+			createdButtons.Add(realButton);
+
 			angle -= dAngle;
 		}
 	}
 
 	public void hide() {
-		foreach (Object obj in createdButtons) {
+		isShown = false;
+		foreach (GameObject obj in createdButtons) {
 			Destroy(obj);
 		}
 		createdButtons.Clear();
@@ -48,7 +61,7 @@ public class NodeMenu : MonoBehaviour {
 
 	void OnMouseExit() {
 		if (!isActionSelected) {
-			hide();
+			//hide();
 		}
 	}
 }
