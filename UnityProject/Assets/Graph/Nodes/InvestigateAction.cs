a769db84f3b49f198adcef31e0f64213d9d8f4b0
knexer <knexer@gmail.com>
2014-04-26 21:31:35 -0700

    Reimplement InvestigateAction
    It's less one-liney, more debuggable

--- UnityProject/Assets/Graph/Nodes/InvestigateAction.cs
+++ UnityProject/Assets/Graph/Nodes/InvestigateAction.cs
@@ -15,10 +15,15 @@ namespace Assets.Graph.Nodes
         //can target any edge that does not belong to you
         public override List<Highlightable> getPossibleTargets()
         {
-            return (List<Highlightable>) new List<EdgeData>(FindObjectsOfType<EdgeData>()).Where<EdgeData>(
+            List<EdgeData> allEdges = new List<EdgeData>(FindObjectsOfType<EdgeData>());
+            List<EdgeData> notMyEdges = allEdges.Where<EdgeData>(
                 (x) => EdgeData.EdgeDirection.Neutral != x.direction 
                     && x.nodeOne.GetComponent<NodeData>().Owner != GetComponent<NodeData>().Owner
-                );
+                ).ToList<EdgeData>();
+
+            Debug.Log(notMyEdges[0]);
+
+            return notMyEdges.Select<EdgeData, Highlightable>((x) => (Highlightable) x).ToList<Highlightable>();
         }
 
         protected override void doActivate(Highlightable target)
