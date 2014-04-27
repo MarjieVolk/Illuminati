f5f3c88e941561f2f3aed2fbdb6f0d81e1748ce1
knexer <knexer@gmail.com>
2014-04-26 21:02:45 -0700

    Implement investigate action
    Untested as of yet

---
+++ UnityProject/Assets/Graph/Nodes/InvestigateAction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+using Assets.Graph;
+
+namespace Assets.Graph.Nodes
+{
+    public class InvestigateAction : Action
+    {
+        public float minVisibilityIncrease, maxVisibilityIncrease;
+
+        //can target any edge that does not belong to you
+        public override List<Highlightable> getPossibleTargets()
+        {
+            return (List<Highlightable>) new List<EdgeData>(FindObjectsOfType<EdgeData>()).Where<EdgeData>(
+                (x) => EdgeData.EdgeDirection.Neutral != x.direction 
+                    && x.nodeOne.GetComponent<NodeData>().Owner != GetComponent<NodeData>().Owner
+                );
+        }
+
+        protected override void doActivate(Highlightable target)
+        {
+            //pick a random amount to increase visibility by
+            float visibilityIncreaseAmount = UnityEngine.Random.value * (maxVisibilityIncrease - minVisibilityIncrease) + minVisibilityIncrease;
+
+            ((EdgeData)target).Visibility += visibilityIncreaseAmount;
+        }
+    }
+}
