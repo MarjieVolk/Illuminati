d80ebae513767fba3198eea090c6918cf1877cf9
knexer <knexer@gmail.com>
2014-04-28 00:00:31 -0700

    Tweaked EndTurnButton's position

--- UnityProject/Assets/HUD/EndTurnButton.cs
+++ UnityProject/Assets/HUD/EndTurnButton.cs
@@ -7,7 +7,7 @@ public class EndTurnButton : HUDButton {
 
 	void Awake () {
 		x = 0.01f;
-		y = 0.135f;
+		y = 0.157f;
 	}
 	
 	override public bool viewAsOwned(VisibilityController.Visibility visibility) {
