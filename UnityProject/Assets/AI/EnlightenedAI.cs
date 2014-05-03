1b6b8acf3f40bb0a2019db0e57094e3919574322
knexer <knexer@gmail.com>
2014-05-03 16:28:49 -0700

    Add basic skeleton types for the AI

---
+++ UnityProject/Assets/AI/EnlightenedAI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.AI
+{
+    public abstract class EnlightenedAI : MonoBehaviour
+    {
+        public abstract List<Action> scheduleActions(int numActionsToSchedule);
+    }
+}
---
+++ UnityProject/Assets/AI/GreedyActionConsumerAI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.AI
+{
+    public class GreedyActionConsumerAI : EnlightenedAI
+    {
+        //has a list of strategies, in priority order
+        //it can schedule orders
+        //it can trigger execute actions / end of turn
+        public ActionConsumptionStrategy[] strategies;
+
+        public override List<Action> scheduleActions(int numActionsToSchedule)
+        {
+            List<Action> ret = new List<Action>();
+            foreach (ActionConsumptionStrategy strategy in strategies)
+            {
+                List<Action> actions = strategy.consumeActions(numActionsToSchedule);
+
+                ret.AddRange(actions);
+                numActionsToSchedule -= actions.Count;
+
+                if (numActionsToSchedule == 0) break;
+            }
+
+            return ret;
+        }
+    }
+}
