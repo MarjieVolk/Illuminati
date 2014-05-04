bd66e7d830d9242661dce24f991d91c92657237e
knexer <knexer@gmail.com>
2014-05-03 17:07:46 -0700

    AI strategy for investing as much as possible

---
+++ UnityProject/Assets/AI/ActionConsumptionStrategies/GreedyInvest.cs
@@ -0,0 +1,33 @@
+﻿using Assets.Graph.Nodes;
+using Assets.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.AI.ActionConsumptionStrategies
+{
+    public class GreedyInvest : ActionConsumptionStrategy
+    {
+        public override List<Action> consumeActions(int numActionsRemaining)
+        {
+            List<Action> ret = new List<Action>();
+            List<NodeData> ownedNodes = FindObjectsOfType<NodeData>().Where<NodeData>((node) => node.Owner == TurnController.instance.CurrentPlayer).ToList<NodeData>();
+
+            foreach (NodeData node in ownedNodes)
+            {
+                NodeMenu menu = node.GetComponent<NodeMenu>();
+                if (!menu.isScheduled)
+                {
+                    InvestAction action = node.gameObject.GetComponent<InvestAction>();
+                    if (action != null)
+                    {
+                        ret.Add(action);
+                    }
+                }
+            }
+
+            return ret;
+        }
+    }
+}
