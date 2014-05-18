6b5094aa11364cf647d3667b306bb7fe4e7246bf
knexer <knexer@gmail.com>
2014-05-18 10:55:05 -0700

    Make everything a child of one gameobject
    As an alternative to singletons

---
+++ UnityProject/Assets/Player/PlayerPutter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Player
+{
+    public class PlayerPutter : MonoBehaviour
+    {
+        private void Start()
+        {
+            foreach (PlayerData player in FindObjectsOfType<PlayerData>())
+            {
+                player.transform.parent = gameObject.transform;
+            }
+        }
+    }
+}
