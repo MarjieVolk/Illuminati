3ab47242a4345718ffcd2a918b3a23f8c4d5f2a0
knexer <knexer@gmail.com>
2014-05-18 14:29:29 -0700

    Add base type for game components

---
+++ UnityProject/Assets/GameLogicComponent.cs
@@ -0,0 +1,36 @@
+﻿using Assets.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets
+{
+    public class GameLogicComponent : MonoBehaviour
+    {
+        private Dictionary<Type, Component> dependencies;
+
+        public TurnController TurnController { get { return getDependency<TurnController>(); } }
+        public GraphUtility GraphUtility { get { return getDependency<GraphUtility>(); } }
+
+        private void Awake()
+        {
+            dependencies = new Dictionary<Type, Component>();
+        }
+
+        private T getDependency<T>() where T : Component
+        {
+            if (!dependencies.ContainsKey(typeof(T)))
+            {
+                dependencies[typeof(T)] = resolveDependency<T>();
+            }
+            return (T) dependencies[typeof(T)];
+        }
+
+        private T resolveDependency<T>() where T : Component
+        {
+            return transform.root.GetComponent<T>();
+        }
+    }
+}
